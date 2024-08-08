109b0e4563b075111c650a0d0161f84eca0b7be6
Andrey Zuev <stepgt@mail.ru>
2024-08-08 14:20:06 +1000

    2.2.6 Adding features to the collection class. Providing item counts

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,13 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection('Adam', todos);
 console.clear();
-console.log(`${collection.userName}'s Todo List`);
 //let newId: number = collection.addTodo("Go for run");
 //let todoItem: TodoItem = collection.getTodoById(newId);
 //todoItem.printDetails();
 //collection.addTodo(todoItem);
-collection.removeComplete();
+//console.log(`${collection.userName}'s Todo List`);
+console.log(
+  `${collection.userName}'s Todo List ` + `(${collection.getItemCounts().incomplete} items to do)`,
+);
+//collection.removeComplete();
 collection.getTodoItems(true).forEach((item) => item.printDetails());
