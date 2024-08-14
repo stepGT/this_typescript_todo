92be70600cbceddb0f0d2099887378e3b839588e
Andrey Zuev <stepgt@mail.ru>
2024-08-14 10:46:36 +1000

    2.4.1 Filtering items

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,18 @@ let todos: TodoItem[] = [
 ];
 
 let collection: TodoCollection = new TodoCollection('Adam', todos);
+let showCompleted = true;
 
 function displayTodoList(): void {
   console.log(
     `${collection.userName}'s Todo List ` +
       `(${collection.getItemCounts().incomplete} items to do)`,
   );
-  collection.getTodoItems(true).forEach((item) => item.printDetails());
+  collection.getTodoItems(showCompleted).forEach((item) => item.printDetails());
 }
 
 enum Commands {
+  Toggle = 'Show/Hide Completed',
   Quit = 'Quit',
 }
 
@@ -34,8 +36,11 @@ function promptUser(): void {
       choices: Object.values(Commands),
     })
     .then((answers) => {
-      if (answers['command'] !== Commands.Quit) {
-        promptUser();
+      switch (answers['command']) {
+        case Commands.Toggle:
+          showCompleted = !showCompleted;
+          promptUser();
+          break;
       }
     });
 }
